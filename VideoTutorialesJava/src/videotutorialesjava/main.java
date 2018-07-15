e9d2736064ed5e15600fd4ccdc13baa0bd3de3a0
mroque <mpc4mirl@gmail.com>
2018-07-14 23:04:33 -0500

    006A Llenar JTable en Java con Netbeans 8.2

--- VideoTutorialesJava/src/videotutorialesjava/main.java
+++ VideoTutorialesJava/src/videotutorialesjava/main.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package videotutorialesjava;
 
 import Datos.Conexion;
 
 /**
  *
- * @author Miler
+ * @author mrrecursos.com
  */
 public class main {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
        new Principal().setVisible(true);            
         /*Conexion c = new Conexion();

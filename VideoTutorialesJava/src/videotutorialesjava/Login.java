ecdf25138f889e65c26b2c69c1678cc0b29313c0
milerroque <mpc-mirl@hotmail.com>
2018-07-14 22:29:31 -0500

    JTable

--- VideoTutorialesJava/src/videotutorialesjava/Login.java
+++ VideoTutorialesJava/src/videotutorialesjava/Login.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package videotutorialesjava;
 
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author Miler
+ * @author mrrecursos.com
  */
 public class Login extends javax.swing.JFrame {
-
-    /**
-     * Creates new form Login
-     */
+   
     public Login() {
         initComponents();
         this.setLocationRelativeTo(this);
